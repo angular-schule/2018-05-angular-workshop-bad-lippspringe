b12776bd5b9f6bc086a464d8f7c08c820fada29e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-05-08 17:01:11 +0200

    using mocks for BookRatingService Spec

--- book-rating/src/app/book/book.component.spec.ts
+++ book-rating/src/app/book/book.component.spec.ts
@@ -1,3 +1,4 @@
+import { BookRatingService } from './../shared/book-rating.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
@@ -6,9 +7,20 @@ describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
+  const ratingMock = {
+    rateUp: () => {},
+    rateDown: () => {},
+    rateDownAllowed: () => {},
+    rateUpAllowed: () => {},
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [ BookComponent ],
+      providers: [{
+        provide: BookRatingService,
+        useValue: ratingMock
+      }]
     })
     .compileComponents();
   }));
@@ -16,6 +28,15 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    // fill the @Input
+    component.book = {
+      isbn: '00',
+      title: '',
+      description: '',
+      rating: 1
+    };
+
     fixture.detectChanges();
   });
 
