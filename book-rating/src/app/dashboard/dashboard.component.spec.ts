d77e5572115bb1dab93f3543173c9225eeeea033
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-05-08 16:30:03 +0200

    integration test

--- book-rating/src/app/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,8 @@
+import { BookComponent } from './../book/book.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +10,13 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [
+        DashboardComponent,
+        BookComponent
+      ],
+      providers: [
+        BookRatingService
+      ]
     })
     .compileComponents();
   }));
