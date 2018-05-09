b78cec08ab37e2aa5dee503b750376ef09455ce2
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-05-09 12:48:03 +0200

    reactive forms

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BookRatingService } from './shared/book-rating.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { CreateBookComponent } from './create-book/create-book.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    CreateBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-    // TODO: FormsModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     BookRatingService
---
+++ book-rating/src/app/create-book/create-book.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'br-create-book',
+  templateUrl: './create-book.component.html',
+  styleUrls: ['./create-book.component.scss']
+})
+export class CreateBookComponent implements OnInit {
+
+  bookForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.bookForm = new FormGroup({
+      isbn: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10)
+      ]),
+      title: new FormControl('', Validators.required),
+      description: new FormControl('')
+    });
+  }
+
+  isInvalid(name: string) {
+    const control = this.bookForm.get(name);
+    return control.invalid && control.dirty;
+  }
+
+}
